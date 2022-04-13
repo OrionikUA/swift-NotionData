9a7958b7868f38d2ce3cf9143414812c16ac3474
Ihor Popkov <ihor.popkov@gmail.com>
2022-04-13 17:32:43 +0300

    public

--- Sources/NotionData/JsonParseObject.swift
+++ Sources/NotionData/JsonParseObject.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct JsonParseObject {
     public let obj: [String: Any]
-    let path: [String]
+    public let path: [String]
     
     public init(obj: [String: Any], path: [String]) {
         self.obj = obj
