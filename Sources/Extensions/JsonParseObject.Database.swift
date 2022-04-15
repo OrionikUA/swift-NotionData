189a8b1220b761ba750d21303cdf0895165ad494
Ihor Popkov <ihor.popkov@gmail.com>
2022-04-15 18:39:31 +0300

    parse Text

--- Sources/Extensions/JsonParseObject.Database.swift
+++ Sources/Extensions/JsonParseObject.Database.swift
@@ -2,12 +2,23 @@
 import Foundation
 
 public extension JsonParseObject {
-    func parseTitle(columnName: String) throws -> String {
+    func parseTitleProperty(columnName: String) throws -> String {
         let obj = try self.parseObject(name: columnName)
         let titles = try obj.parseArray(name: NotionNodes.title)
         var titleStr: String = ""
         for title in titles {
             let text = try title.parseString(name: NotionNodes.plainText)
+            titleStr = titleStr + text
+        }
+        return titleStr
+    }
+    
+    func parseTextProperty(columnName: String) throws -> String {
+        let obj = try self.parseObject(name: columnName)
+        let richTexts = try obj.parseArray(name: NotionNodes.richText)
+        var titleStr: String = ""
+        for richText in richTexts {
+            let text = try richText.parseString(name: NotionNodes.plainText)
             titleStr = titleStr + text
         }
         return titleStr
