7933e181aafed1d5ca0e84ee95c8b4a93a3aa980
Ihor Popkov <ihor.popkov@gmail.com>
2022-07-21 19:31:44 +0300

    Add filter condition. Refactor

--- Sources/Models/NotionFilterCondition.swift
+++ Sources/Models/NotionFilterCondition.swift
@@ -4,4 +4,5 @@ public enum NotionFilterCondition {
     public static let equals = "equals"
     public static let contains = "contains"
     public static let after = "after"
+    public static let before = "before"
 }
